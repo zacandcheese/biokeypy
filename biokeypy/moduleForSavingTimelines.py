7dae1ccee5ae8864affcb0ffb47c934d7ac1e1d1
Zach Nowak <zacandcheese@icloud.com>
2018-02-22 10:01:28 -0500

    ADD: A module which saves timelines
    Currently 2/22/18
    _______________________________
    -It saves the data into two dictionaries.

---
+++ biokeypy/moduleForSavingTimelines.py
@@ -0,0 +1,23 @@
+import json
+import platform
+import os
+def saveTimeLine(pressTime, charTime, person,location):
+	
+	os.chdir("lib/")
+	
+	filename =  location + person + "A.txt"#GREEN computer
+	filename2 = location + person + "B.txt"#GREEN computer
+	outfile = open(filename, 'w')
+	outfile2 = open(filename2, 'w')
+	Dict1 = {}
+	i = 0
+	for num in pressTime:
+		Dict1[i] = num
+		i += 1
+	Dict2 = {}
+	i = 0
+	for char in charTime:
+		Dict2[i] = char
+		i += 1
+	json.dump(Dict1, open(filename, 'w'))
+	json.dump(Dict2, open(filename2, 'w'))
