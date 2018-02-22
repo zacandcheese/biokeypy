ee3261c5d5e2873d7c2401fe14f32cb05ad4ef4a
Zach Nowak <zacandcheese@icloud.com>
2018-02-22 09:57:49 -0500

    ADD: Add a module which converts data into something
    Currently 2/22/18
    ______________________________
    - Can convert timelines into a file which can be used in multiple ways

---
+++ biokeypy/moduleForDeconstructingTimelines.py
@@ -0,0 +1,73 @@
+import glob
+import os
+import json
+import statistics
+import string
+import platform
+
+
+def makeTable(intDict, charDict, location, person):
+	
+	"""
+	list of all tuples found in what the person typed
+	# of appearances, median, variance
+	"""
+	totalSentence = ""
+	for i in range(len(charDict)):
+		totalSentence += charDict[str(i)]
+		
+	print(totalSentence)
+
+	filename = location + person + ".txt"#GREEN computer
+	
+	listOfTuples = []
+	#cycle all letters
+	for i in ([""]+list(string.ascii_lowercase)):
+		for j in ([""]+list(string.ascii_lowercase)):
+			for k in (list(string.ascii_lowercase)):
+				for l in (list(string.ascii_lowercase)):
+					tuple = i+j+k+l
+					
+					if(tuple not in listOfTuples):
+						
+						if tuple in totalSentence.lower():
+							allTimes = []
+							for m in range(len(totalSentence)-len(tuple)):
+								pTuple = ""
+								for n in range(len(tuple)):
+									pTuple += totalSentence[(m+n)].lower()
+								if (pTuple == tuple):
+									allTimes.append(intDict[str(m+len(tuple)-1)]-intDict[str(m)])
+									
+							#ADD IT TO FILE
+							if len(allTimes)>=3:
+								listOfTuples.append(tuple)
+								print(tuple,len(allTimes),statistics.mean(allTimes),statistics.median(allTimes), statistics.variance(allTimes))
+								dummyFile = open(filename, 'a')
+								dummyFile.write(str(tuple)+","+str(len(allTimes))+","+str(statistics.mean(allTimes))+","+str(statistics.median(allTimes))+","+str(statistics.variance(allTimes))+"\n")
+						#The entire sentence of what they wrote
+						#list of every appearances, time for each
+	
+
+def userSummary(fileName, location):
+	#os.chdir(location)
+	print("!!!")
+	listOfTxtFiles = []
+	for file in glob.glob(location+"/*.txt"):#CHANGE
+		listOfTxtFiles.append(file)
+
+	listOfTxtFiles = sorted(listOfTxtFiles, key=str.lower)
+	print(listOfTxtFiles)
+	
+	newListOfTxtFiles = []
+	for file in listOfTxtFiles:
+		if(fileName in file):
+			newListOfTxtFiles.append(file)
+	
+	numFiles = round(len(newListOfTxtFiles)/2)
+	print(newListOfTxtFiles)
+	for num in range(int(numFiles)):
+		intDict = json.load(open(newListOfTxtFiles[num*2],'r'))
+		charDict = json.load(open(newListOfTxtFiles[num*2+1],'r'))
+		makeTable(intDict, charDict,(location+"/Summary/"), fileName)
+		print("\n")
